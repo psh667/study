f7c4d252c23ead56692ce75ca26ea4c94f80e3e8
psh <paksan@nate.com>
2013-07-25 17:44:28 +0900

    setting load /save 추가

--- JQTouch/kilo.js
+++ JQTouch/kilo.js
@@ -4,9 +4,28 @@ var jQT = $.jQTouch({
 
 $(document).ready(function(){
     $('#settings form').submit(saveSettings);
+    $('#settings').bind('pageAnimationStart', loadSettings);//loadSettings();
 });
 function saveSettings() {
+    localStorage.age = $('#age').val();
+    localStorage.budget = $('#budget').val();
+    localStorage.weight = $('#weight').val(); 
     jQT.goBack();
     return false;
 }
 
+
+function loadSettings() {
+    if (!localStorage.age) {
+        localStorage.age = ""; 
+    }
+    if (!localStorage.budget) { 
+        localStorage.budget = "";
+    }
+    if (!localStorage.weight) {
+        localStorage.weight = ""; 
+    }
+    $('#age').val(localStorage.age);
+    $('#budget').val(localStorage.budget);
+    $('#weight').val(localStorage.weight);
+}
