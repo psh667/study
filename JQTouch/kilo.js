b2ce6ac606af0287e402da1790797833a4faef74
psh <paksan@nate.com>
2013-07-25 17:58:44 +0900

    date 제목에 날짜 표시하기 localStorage 사용

--- JQTouch/kilo.js
+++ JQTouch/kilo.js
@@ -5,6 +5,18 @@ var jQT = $.jQTouch({
 $(document).ready(function(){
     $('#settings form').submit(saveSettings);
     $('#settings').bind('pageAnimationStart', loadSettings);//loadSettings();
+
+    $('#dates li a').bind('click touchend', function(){
+        var dayOffset = this.id;//0: today, 1: yesterday, 2: 2days ago
+        var date = new Date();
+        date.setDate(date.getDate() - dayOffset);
+        sessionStorage.currentDate = date.getMonth() + 1 + '/' + 
+                                     date.getDate() + '/' + 
+                                     date.getFullYear();
+	alert( sessionStorage.currentDate );
+	refreshEntries();
+    });
+
 });
 function saveSettings() {
     localStorage.age = $('#age').val();
@@ -29,3 +41,9 @@ function loadSettings() {
     $('#budget').val(localStorage.budget);
     $('#weight').val(localStorage.weight);
 }
+
+function refreshEntries() {
+    var currentDate = sessionStorage.currentDate;
+    $('#date h1').text( currentDate );
+}
+
