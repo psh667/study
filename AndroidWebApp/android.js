cb6c93aaa92e9b1db60158181bd2bb3dfa9d0ab4
psh <paksan@nate.com>
2013-07-25 14:29:19 +0900

    JQTouch 적용 마무리

--- AndroidWebApp/android.js
+++ AndroidWebApp/android.js
@@ -1,10 +1,10 @@
 //if (window.innerWidth && window.innerWidth <= 480) { 
     $(document).ready(function(){
         $('#header ul').addClass('hide'); 
-        $('#header').append('<div class="leftButton" onclick="toggleMenu()">Menu</div>');
+        $('#header').append('<div class="button slideup" onclick="toggleMenu()">Menu</div>');
     }); 
     function toggleMenu() {
         $('#header ul').toggleClass('hide'); 
-        $('#header .leftButton').toggleClass('pressed');
+	$('#header .leftButton').toggleClass('pressed');
     }
 //}
